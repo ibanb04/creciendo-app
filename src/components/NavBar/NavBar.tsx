5231ceddb626b3829b94a12517865d69dd822da8
ibanb04 <ibanb040@gmail.com>
2022-09-19 15:42:49 -0500

    Correccion boton menu navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -76,8 +76,8 @@ export const NavBar: FC<NavBarProps> = ({
                   <IconButton
                     size="large"
                     aria-label="account of current user"
-                    
-                 
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
                     {...bindTrigger(popupState)}
                     color="inherit"
                   >
