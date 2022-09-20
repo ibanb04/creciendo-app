7e160e1e6e04b7e629cd3ac41ce1b4707b2f1323
JesusRamosV <dev.jesusramos@gmail.com>
2022-09-20 01:58:11 -0500

    Ajustes en el DashboardRoutes

--- src/components/routers/DashboardRoutes.tsx
+++ src/components/routers/DashboardRoutes.tsx
@@ -1,61 +1,42 @@
-import { styled } from "@mui/material/styles";
-import { Box, Grid } from "@mui/material";
-import { useState } from "react";
+import { Box, Grid, Toolbar } from "@mui/material";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Sidebar } from "../Sidebar";
 import { RegisterStudent } from "../RegisterStudent";
 import { NavBar } from "../NavBar";
+import { useState } from "react";
 
 const drawerWidth = 250;
 
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create("margin", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
 export const DashboardRoutes = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
+  let xs;
+
+  if (open) {
+    xs = 9;
+  } else {
+    xs = 11;
+  }
 
   return (
     <>
       <Box sx={{ display: "flex" }}>
-        <Grid container>
+        <Grid container direction="row" columnSpacing={5}>
           <Grid item xs={12}>
             <NavBar open={open} drawerWidth={drawerWidth} setOpen={setOpen} />
           </Grid>
-          <Grid xs={4}>
+          <Grid item xs={xs - 10}>
             <Sidebar open={open} drawerWidth={drawerWidth} setOpen={setOpen} />
           </Grid>
-          <Grid xs={8}>
-            <Main open={open}>
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/sidebar" element={<RegisterStudent />} />
-                </Routes>
-              </BrowserRouter>
-            </Main>
+          <Grid item xs>
+            <Toolbar />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/sidebar" element={<RegisterStudent />} />
+              </Routes>
+            </BrowserRouter>
           </Grid>
         </Grid>
       </Box>
-      {/* <Router>
-        <div>
-          <Route path="/sidebar" component={Sidebar} />
-        </div>
-      </Router> */}
     </>
   );
 };
