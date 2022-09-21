5bbdc438622e0627c720df02de69660d4c806a98
JesusRamosV <dev.jesusramos@gmail.com>
2022-09-20 23:53:24 -0500

    Se agregó la logica para las rutas públicas y privadas, se añadió el contexto para la persistencia de sesión con localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { ThemeProvider } from "@emotion/react";
+import { useEffect, useReducer } from "react";
+import { AuthContext } from "./auth/authContext";
+import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./components/routers/AppRouter";
 import { theme } from "./shared/themeConfig";
 
 function App() {
+  const init = () => {
+    return JSON.parse(localStorage.getItem("user")!) || { logged: false };
+  };
+
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+
+  useEffect(() => {
+    if (!user) return;
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
   return (
-    <ThemeProvider theme={theme}>
-      <AppRouter />
-    </ThemeProvider>
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <ThemeProvider theme={theme}>
+        <AppRouter />
+      </ThemeProvider>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,21 @@
+import { Button } from "@mui/material";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
 
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const { dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const lastPath = localStorage.getItem("lastPath") || "/";
+  const handleLogin = () => {
+    dispatch({
+      type: "login",
+      payload: {
+        name: "Jesus Ramos",
+      },
+    });
+
+    navigate(lastPath, { replace: true });
+  };
+  return <Button onClick={handleLogin}>Login</Button>;
+};
