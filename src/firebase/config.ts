8733d01c27e52c48e791219c66f9f90078e5f4a4
ibanb04 <ibanb040@gmail.com>
2022-11-17 09:25:21 -0500

    Guardar el rol de los usuarios en firestore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore, } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
