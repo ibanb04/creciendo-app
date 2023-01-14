110f0f95bb04305633f97dbb2ed3af4faddb04b3
ibanb04 <ibanb040@gmail.com>
2023-01-14 09:00:02 -0500

    Actualización del año lectivo

--- src/hooks/useLectiveYear.ts
+++ src/hooks/useLectiveYear.ts
@@ -3,6 +3,9 @@ import moment from "moment";
 export const useLectiveYear = () => {
     const month = moment().format('MM');
     const lectiveYear = new Date().getFullYear();
-    if (month === '01') return lectiveYear;
+    if (month === '01' || month === '02' ||
+        month === '03' || month === '04' ||
+        month === '05' || month === '06' ||
+        month === '07') return lectiveYear;
     else return lectiveYear + 1;
 }
